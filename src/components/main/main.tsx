0279ca2a72c70fa90d6a95e43dffa522a83964a0
Leonardo <leonardorscarpitta@gmail.com>
2024-07-03 14:24:51 -0300

    feat: add faq section

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -1,15 +1,30 @@
 import "./main.modules.scss"
-import CardLoja from "./loja/card-loja"
+import CardLoja from "./card/card-loja"
+import VideoSection from "./video-section/video-section"
+import Faq from "./faq/faq"
 
 export default function Main() {
   return (
     <main>
-      <h2>Loja</h2>
-      <div className="cards">
-        <CardLoja url="https://img.icons8.com/clouds/512/minecraft-grass-cube.png" size={256} altText="Icone de card" text="Compre já!"/>
-        <CardLoja url="https://img.icons8.com/clouds/512/minecraft-grass-cube.png" size={256} altText="Icone de card"  text="Compre já!"/>
-        <CardLoja url="https://img.icons8.com/clouds/512/minecraft-grass-cube.png" size={256} altText="Icone de card"  text="Compre já!"/>
-      </div>
+
+      <VideoSection />
+
+      <h2 className="underline">Loja</h2>
+      <section className="cards">
+        <CardLoja url="https://img.icons8.com/stickers/512/minecraft-pickaxe.png" altText="Icone de card" text="Lorem"/>
+        <CardLoja url="https://img.icons8.com/color/512/badlion.png" altText="Icone de card"  text="Lorem"/>
+        <CardLoja url="https://img.icons8.com/bubbles/512/master-craft.png" altText="Icone de card"  text="Lorem"/>
+        <CardLoja url="https://img.icons8.com/cute-clipart/512/lunar-client.png" altText="Icone de card"  text="Lorem"/>
+        <CardLoja url="https://img.icons8.com/clouds/512/minecraft-grass-cube.png" altText="Icone de card"  text="Lorem"/>
+      </section>
+
+      <section className="faq">
+        <h2 className="underline">Perguntas Frequentes</h2>
+        <Faq question="Lorem" answer="Lorem Lorem Lorem Lorem Lorem" />
+        <Faq question="Lorem" answer="Lorem Lorem Lorem Lorem Lorem" />
+        <Faq question="Lorem" answer="Lorem Lorem Lorem Lorem Lorem" />
+        <Faq question="Lorem" answer="Lorem Lorem Lorem Lorem Lorem" />
+      </section>
     </main>
   )
 }
