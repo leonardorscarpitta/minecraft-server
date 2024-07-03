75794bac9c2cf17d4a890bd2f9d218c452911754
Leonardo <leonardorscarpitta@gmail.com>
2024-07-03 11:01:40 -0300

    perf: optmize icon from login button

--- src/components/header/login.tsx
+++ src/components/header/login.tsx
@@ -1,7 +1,13 @@
+import Image from "next/image"
+
 export default function Login() {
   return (
     <div>
-      <button>Login</button>
+      <button>
+        <Image src="https://img.icons8.com/plasticine/256/minecraft-optifine.png"
+        width={30} height={30} alt="Logo da aba de login" quality={100} />
+        <p>Login</p>
+      </button>
     </div>
   )
 }
