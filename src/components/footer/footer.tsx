628206ad229d97e9c4a5e1120e21222e85539860
Leonardo <leonardorscarpitta@gmail.com>
2024-07-04 13:31:06 -0300

    build: create a footer

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,9 @@
+import styles from "./footer.module.scss"
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <h3>@2024 - Todos os direitos reservados</h3>
+    </footer>
+  )
+}
